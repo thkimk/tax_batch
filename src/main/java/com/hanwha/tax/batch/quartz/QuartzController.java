4112434028053283ace72e998caecb9dfdc9be98
hjlee <asd3gw@gmail.com>
2022-11-07 17:08:20 +0900

    1. 필요없는 주석 제거

--- src/main/java/com/hanwha/tax/batch/quartz/QuartzController.java
+++ src/main/java/com/hanwha/tax/batch/quartz/QuartzController.java
@@ -19,9 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @Slf4j
 @Controller
