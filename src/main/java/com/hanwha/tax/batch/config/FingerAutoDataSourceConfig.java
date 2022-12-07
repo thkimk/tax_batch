888028299a42dbf279bf8d4de7c436710c3b39b9
hjlee <asd3gw@gmail.com>
2022-12-07 17:28:03 +0900

    1. 메일 레파지토리 필요없는 소스 제거 2. fingerauto 디비 설정 주석처리

--- src/main/java/com/hanwha/tax/batch/config/FingerAutoDataSourceConfig.java
+++ src/main/java/com/hanwha/tax/batch/config/FingerAutoDataSourceConfig.java
@@ -16,43 +16,43 @@ import org.springframework.transaction.PlatformTransactionManager;
 import javax.sql.DataSource;
 import java.util.HashMap;
 
-@Configuration
-@PropertySource({"classpath:application-prd.yml"})
-@EnableJpaRepositories(basePackages = "com.hanwha.tax.batch.fingerauto.repository", entityManagerFactoryRef = "fingerAutoEntityManager", transactionManagerRef = "fingerAutoTransactionManager")
+//@Configuration
+//@PropertySource({"classpath:application-prd.yml"})
+//@EnableJpaRepositories(basePackages = "com.hanwha.tax.batch.fingerauto.repository", entityManagerFactoryRef = "fingerAutoEntityManager", transactionManagerRef = "fingerAutoTransactionManager")
 public class FingerAutoDataSourceConfig {
-	@Autowired
-	private Environment env;
-
-	@Bean
-	public LocalContainerEntityManagerFactoryBean fingerAutoEntityManager() {
-		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-		em.setDataSource(fingerDataSource());
-
-		//Entity 패키지 경로
-		em.setPackagesToScan(new String[] { "com.hanwha.tax.batch.entity.finger" });
-
-		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-		em.setJpaVendorAdapter(vendorAdapter);
-
-		//Hibernate 설정
-		HashMap<String, Object> properties = new HashMap<>();
-		properties.put("hibernate.hbm2ddl.auto",env.getProperty("hibernate.hbm2ddl.auto"));
-		properties.put("hibernate.dialect",env.getProperty("hibernate.dialect"));
-		properties.put("hibernate.physical_naming_strategy","com.hanwha.tax.batch.config.hibernate.ImprovedNamingStrategy");
-		em.setJpaPropertyMap(properties);
-		return em;
-	}
-
-	@Bean
-	@ConfigurationProperties(prefix="spring.finger-auto-ds")
-	public DataSource fingerDataSource() {
-		return DataSourceBuilder.create().build();
-	}
-
-	@Bean
-	public PlatformTransactionManager fingerAutoTransactionManager() {
-		JpaTransactionManager transactionManager = new JpaTransactionManager();
-		transactionManager.setEntityManagerFactory(fingerAutoEntityManager().getObject());
-		return transactionManager;
-	}
+//	@Autowired
+//	private Environment env;
+//
+//	@Bean
+//	public LocalContainerEntityManagerFactoryBean fingerAutoEntityManager() {
+//		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+//		em.setDataSource(fingerDataSource());
+//
+//		//Entity 패키지 경로
+//		em.setPackagesToScan(new String[] { "com.hanwha.tax.batch.entity.finger" });
+//
+//		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//		em.setJpaVendorAdapter(vendorAdapter);
+//
+//		//Hibernate 설정
+//		HashMap<String, Object> properties = new HashMap<>();
+//		properties.put("hibernate.hbm2ddl.auto",env.getProperty("hibernate.hbm2ddl.auto"));
+//		properties.put("hibernate.dialect",env.getProperty("hibernate.dialect"));
+//		properties.put("hibernate.physical_naming_strategy","com.hanwha.tax.batch.config.hibernate.ImprovedNamingStrategy");
+//		em.setJpaPropertyMap(properties);
+//		return em;
+//	}
+//
+//	@Bean
+//	@ConfigurationProperties(prefix="spring.finger-auto-ds")
+//	public DataSource fingerDataSource() {
+//		return DataSourceBuilder.create().build();
+//	}
+//
+//	@Bean
+//	public PlatformTransactionManager fingerAutoTransactionManager() {
+//		JpaTransactionManager transactionManager = new JpaTransactionManager();
+//		transactionManager.setEntityManagerFactory(fingerAutoEntityManager().getObject());
+//		return transactionManager;
+//	}
 }
